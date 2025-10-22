d0d8627840ea8bec88444acc7f6fc6c084a0e8b6
Manuel <manuel.barcena.sto@gmail.com>
2025-10-22 11:43:42 -0300

    Agregar el microservicio de eureka al nuevo repositorio
    Se agrego al nuevo repositorio lo que estaba en el viejo repositorio

---
+++ src/main/java/com/mobydigital/academy/mobyapp/eureka/EurekaApplication.java
@@ -0,0 +1,15 @@
+package com.mobydigital.academy.mobyapp.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
